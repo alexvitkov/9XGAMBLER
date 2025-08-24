c6c09a3069ad51f3e4c8be1ab3095ae4697746ca
GHOST_DISK <alex@panzermail.com>
2025-08-24 20:11:18 +0300

    checkpoint

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,8 @@
 #define MACHINE_GAP_Y 18
 #define TOP_PADDING 18
 #define RIGHT_PADDING 18
-#define BUTTON_SIZE 48
+#define BUTTON_WIDTH 60
+#define BUTTON_HEIGHT 48
 
 struct Machine {
     int x = 0;
@@ -26,24 +27,70 @@ struct Machine {
     virtual void update() = 0;
 };
 
-struct M3x1 : Machine {
-    virtual void update() {
-        DrawRectangle(x, y, MACHINE_WIDTH, MACHINE_HEIGHT, RED);
-
-        DrawRectangle(x + (MACHINE_WIDTH - BUTTON_SIZE) / 2, y + (MACHINE_HEIGHT - BUTTON_SIZE) - 8, BUTTON_SIZE, BUTTON_SIZE, BLUE);
-    };
-};
-
-
 const char* game_title = "9X GAMBLER";
 int screen_width = 1024;
 int screen_height = 768;
 float screen_scale;
+Vector2 mouse;
 
 Texture tex_background;
+Texture tex_m3x1;
 
 // --- Gameplay State -----------------------------------------
 Machine* machines[9] = {};
+int64_t money = 100;
+
+struct Slot {
+    bool spinning = false;
+
+    void spin() {
+        spinning = true;
+    }
+
+    void update() {
+    }
+};
+
+struct M3x1 : Machine {
+    Slot slot = {};
+
+    virtual void update() {
+        slot.update();
+
+        DrawRectangle(x, y, MACHINE_WIDTH, MACHINE_HEIGHT, RED);
+
+        Rectangle button = {
+            .x = x + float(MACHINE_WIDTH - BUTTON_WIDTH) / 2,
+            .y = y + float(MACHINE_HEIGHT - BUTTON_HEIGHT) - 8,
+            .width = float(BUTTON_WIDTH),
+            .height = float(BUTTON_HEIGHT),
+        };
+
+        Color color = { 0, 0, 255, 255 };
+
+        if (slot.spinning) {
+            button.y += 12;
+            button.height -= 12;
+            color = { 0, 0, 160, 180 };
+        }
+        else {
+            if (CheckCollisionPointRec(mouse, button)) {
+                color = Color { 32, 80, 255, 255 };
+
+                if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                    slot.spin();
+                }
+            }
+        }
+
+        DrawTexture(tex_m3x1, x, y - 9, WHITE);
+
+        DrawRectangleRec(button, color);
+        DrawText("SPIN", button.x + 6, button.y + 10, 20, WHITE);
+    };
+};
+
+
 
 int main() {
 
@@ -53,6 +100,7 @@ int main() {
 
     // --- Load Assets --------------------------------------------
     tex_background = LoadTexture("assets/background.png");
+    tex_m3x1 = LoadTexture("assets/m3x1.png");
 
     M3x1* m3x1 = new M3x1();
     machines[0] = m3x1;
@@ -84,12 +132,12 @@ int main() {
 
         }
 
-
-
         BeginDrawing();
 
         BeginMode2D(camera);
         ClearBackground(BLACK);
+
+        mouse = GetScreenToWorld2D(GetMousePosition(), camera);
 
         // Render Game
         {
@@ -101,7 +149,7 @@ int main() {
                 int x = TOP_PADDING + (i % 3) * (MACHINE_WIDTH + MACHINE_GAP_X);
                 int y = RIGHT_PADDING + (i / 3) * (MACHINE_HEIGHT + MACHINE_GAP_Y);
 
-                DrawRectangle(x, y, MACHINE_WIDTH, MACHINE_HEIGHT, Color{32,32,32,255});
+                DrawRectangle(x, y, MACHINE_WIDTH, MACHINE_HEIGHT, Color{0, 0, 0, 90});
 
                 if (machine) {
                     machine->x = x;
@@ -109,6 +157,10 @@ int main() {
                     machine->update();
                 }
             }
+
+            char buf[64];
+            snprintf(buf, 64, "%ld", money);
+            DrawText(buf, 714, 154, 40, WHITE);
         }
 
 
